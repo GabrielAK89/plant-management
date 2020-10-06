eb2f23f72a243c52a6a859e6cb071949a0dc3c92
AdinaAM94 <67585052+AdinaAM94@users.noreply.github.com>
2020-10-06 13:51:22 -0700

    Undo

--- src/App.js
+++ src/App.js
@@ -32,9 +32,7 @@ class App extends Component {
   }
 
   render() {
-    // return <div className="App">{this.state.user ? <Home /> : <Login />}</div>;
-    return <div className="App"> <Home /></div>;
-
+    return <div className="App">{this.state.user ? <Home /> : <Login />}</div>;
   }
 }
 
